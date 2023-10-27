931db455685874afb4cacec113cdeb98f08d24ff
Maxwell L <85193416+Hyde-R@users.noreply.github.com>
2023-10-26 21:55:48 -0300

    Pratica 6 POO

---
+++ Pratica6/src/questao1/Principal.java
@@ -0,0 +1,12 @@
+package questao1;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        JOptionPane.showInputDialog("Qual a data de criação da Rede Social?");
+    }
+}
---
+++ Pratica6/src/questao1/RedeSocial.java
@@ -0,0 +1,61 @@
+package questao1;
+
+import java.util.ArrayList;
+
+public class RedeSocial {
+    private String dataCriacao;
+    private String nomeUsuario;
+    private String dataNascimento;
+    private String senha;
+
+    ArrayList<Publicacao> publicacoes = new ArrayList<>();
+
+    public RedeSocial(String dataCriacao, String nomeUsuario, String dataNascimento, String senha) {
+        this.dataCriacao = dataCriacao;
+        this.nomeUsuario = nomeUsuario;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void inserePublicacao(Publicacao publi){
+        publicacoes.add(publi);
+    }
+
+    public void imprimePublicacao(){
+        for(Publicacao pub : publicacoes){
+            pub.getDataPublicacao();
+        }
+    }
+}
